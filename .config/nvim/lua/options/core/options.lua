846a83b161271b3390211afb3cd7b64a3bbf24f9
Ståle Jacobsen <staale.jacobsen@horde.no>
2025-05-05 19:48:00 +0200

    Add comments explain each line

--- .config/nvim/lua/options/core/options.lua
+++ .config/nvim/lua/options/core/options.lua
@@ -1,34 +1,46 @@
-vim.cmd("let g:netrw_banner = 0")
+vim.cmd("let g:netrw_banner = 0")        -- disable the netrw banner at the top when browsing files
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.nu = true                        -- show absolute line numbers
+vim.opt.relativenumber = true            -- show line numbers relative to the cursor
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
+vim.opt.tabstop = 4                      -- a tab character equates to 4 spaces
+vim.opt.softtabstop = 4                  -- editing ‹Tab› and ‹Backspace› treat 4 spaces as a tab
+vim.opt.shiftwidth = 4                   -- indentation operations shift by 4 spaces
+vim.opt.expandtab = true                 -- convert tabs to spaces
+vim.opt.autoindent = true                -- copy indent from current line when starting a new one
+vim.opt.smartindent = true               -- add extra indent in certain programming constructs
+vim.opt.wrap = false                     -- don't wrap long lines, let them scroll horizontally
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
+vim.opt.swapfile = false                 -- don’t use a swapfile (no .swp files)
+vim.opt.backup = false                   -- don’t keep backup files around
+vim.opt.undofile = true                  -- enable persistent undo so undo history survives restarts
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"        -- use the system clipboard for all yank, delete, change and paste operations
 
-vim.opt.incsearch = true
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.opt.incsearch = true                 -- show search matches as you type
+vim.opt.inccommand = "split"             -- preview substitutions in a split window
+vim.opt.ignorecase = true                -- case-insensitive search…
+vim.opt.smartcase = true                 -- …unless the search pattern has uppercase letters
 
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true             -- enable true-color support in the terminal
+vim.opt.background = "dark"              -- tell color schemes that you have a dark background
+vim.opt.scrolloff = 8                    -- keep 8 lines visible above/below the cursor
+vim.opt.signcolumn = "yes"               -- always reserve space for the sign column
 
+vim.opt.backspace = { "start", "eol", "indent" }  -- make backspace work like most editors
 
-vim.opt.backspace = {"start", "eol"}
+vim.opt.splitright = true                -- force vertical splits to open to the right
+vim.opt.splitbelow = true                -- force horizontal splits to open below
+
+vim.opt.isfname:append("@-@")            -- treat “@-@” as part of filenames (improves gf)
+vim.opt.updatetime = 50                  -- faster CursorHold events (default is 4000ms)
+vim.opt.colorcolumn = "80"               -- highlight column 80 to guide line length
+
+vim.opt.hlsearch = true                  -- highlight all search matches
+
+vim.opt.mouse = "a"                      -- enable mouse in all modes
+vim.g.editorconfig = true                -- enable EditorConfig integration (if plugin is installed)
+
 
 -- Auto-pairing for brackets, braces, and parentheses in Insert mode
 vim.keymap.set('i', '(', '()<Left>', {noremap = true})
